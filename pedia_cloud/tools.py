f8c33a10a082a6b4929e3f7491f842b2e39d06bc
sheiun <me.sheiun@gmail.com>
2020-05-26 21:10:58 +0800

    Delete useless tool

--- pedia_cloud/tools.py
+++
@@ -1,8 +0,0 @@
-from .base import PediaDictionary
-
-
-def get_all_combinations(idiom: str):
-    # cut words
-    words = list(idiom)
-    ants_lst = [PediaDictionary.get_all_annotations(word) for word in words]
-    return ants_lst
