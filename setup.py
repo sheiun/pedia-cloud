6f51d948d38f183a5c0a3c00c47e656e2c1dffc6
sheiun <me.sheiun@gmail.com>
2020-06-23 00:09:23 +0800

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pedia-cloud",
-    version="0.0.3",
+    version="0.0.4",
     author="SheiUn",
     author_email="develop@sheiun.me",
     description="A third-party Python API for Pedia Cloud Dictionary",
