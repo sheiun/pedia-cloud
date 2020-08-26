0ba7e6387e970b72f108e39e5b71a0a38227edf1
SheiUn <me.sheiun@gmail.com>
2020-08-26 12:24:49 +0800

    bump version to 0.10.0

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
-import setuptools
-from pip._internal.req import parse_requirements
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-install_reqs = parse_requirements("requirements.txt", session="hack")
-try:
-    reqs = [str(ir.req) for ir in install_reqs]
-except AttributeError:
-    reqs = [str(ir.requirement) for ir in install_reqs]
-
-setuptools.setup(
+setup(
     name="pedia-cloud",
-    version="0.0.9",
+    version="0.10.0",
     author="SheiUn",
     author_email="develop@sheiun.me",
     description="A third-party Python API for Pedia Cloud Dictionary",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sheiun/pedia-cloud",
-    packages=setuptools.find_packages(),
-    install_requires=reqs,
+    packages=find_packages(),
+    install_requires=["requests"],
     test_suite="tests",
     classifiers=[
         "Programming Language :: Python :: 3",
